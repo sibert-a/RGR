b502fdb345878d553062e06874d8f3cc039062d1
sibert-a <sibert07@mail.ru>
2026-04-15 21:23:01 +0700

    Добавьте файлы проекта.

---
+++ Ргр/MainForm.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace РГР
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+
+            // Настройка формы
+            this.Text = "Визуализация алгоритмов сортировки";
+            this.ClientSize = new System.Drawing.Size(1200, 700);
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.BackColor = System.Drawing.SystemColors.Control;
+
+            // Canvas панель
+            this.canvas = new Panel();
+            this.canvas.Location = new System.Drawing.Point(10, 10);
+            this.canvas.Size = new System.Drawing.Size(1160, 400);
+            this.canvas.BackColor = System.Drawing.Color.White;
+            this.canvas.BorderStyle = BorderStyle.FixedSingle;
+            this.canvas.Paint += new PaintEventHandler(this.Canvas_Paint);
+
+            // Панель управления
+            this.controlPanel = new Panel();
+            this.controlPanel.Location = new System.Drawing.Point(10, 420);
+            this.controlPanel.Size = new System.Drawing.Size(1160, 230);
+            this.controlPanel.BackColor = System.Drawing.Color.LightGray;
+
+            // Метка "Алгоритм"
+            Label algoLabel = new Label();
+            algoLabel.Text = "Алгоритм:";
+            algoLabel.Location = new System.Drawing.Point(10, 15);
+            algoLabel.Size = new System.Drawing.Size(70, 25);
+
+            // ComboBox алгоритмов
+            this.algorithmCombo = new ComboBox();
+            this.algorithmCombo.Location = new System.Drawing.Point(90, 12);
+            this.algorithmCombo.Size = new System.Drawing.Size(180, 25);
+            this.algorithmCombo.DropDownStyle = ComboBoxStyle.DropDownList;
+            this.algorithmCombo.Items.AddRange(new object[] {
+                "Пузырьковая (Bubble Sort)",
+                "Сортировка выбором (Selection Sort)",
+                "Сортировка вставками (Insertion Sort)",
+                "Сортировка слиянием (Merge Sort)",
+                "Быстрая сортировка (Quick Sort)",
+                "Древесная (Tree Sort)"
+            });
+            this.algorithmCombo.SelectedIndex = 0;
+
+            // Метка "Размер"
+            Label sizeLabel = new Label();
+            sizeLabel.Text = "Размер:";
+            sizeLabel.Location = new System.Drawing.Point(300, 15);
+            sizeLabel.Size = new System.Drawing.Size(50, 25);
+
+            // NumericUpDown размера
+            this.sizeNumeric = new NumericUpDown();
+            this.sizeNumeric.Location = new System.Drawing.Point(355, 12);
+            this.sizeNumeric.Size = new System.Drawing.Size(60, 25);
+            this.sizeNumeric.Minimum = 5;
+            this.sizeNumeric.Maximum = 100;
+            this.sizeNumeric.Value = 30;
+            this.sizeNumeric.ValueChanged += new EventHandler(this.SizeNumeric_ValueChanged);
+
+            // Метка "Скорость"
+            Label speedLabel = new Label();
+            speedLabel.Text = "Скорость (мс):";
+            speedLabel.Location = new System.Drawing.Point(450, 15);
+            speedLabel.Size = new System.Drawing.Size(80, 25);
+
+            // TrackBar скорости
+            this.speedTrackBar = new TrackBar();
+            this.speedTrackBar.Location = new System.Drawing.Point(535, 5);
+            this.speedTrackBar.Size = new System.Drawing.Size(200, 40);
+            this.speedTrackBar.Minimum = 1;
+            this.speedTrackBar.Maximum = 100;
+            this.speedTrackBar.Value = 20;
+            this.speedTrackBar.TickFrequency = 10;
+
+            // Кнопка Старт
+            this.startButton = new Button();
+            this.startButton.Text = "Старт";
+            this.startButton.Location = new System.Drawing.Point(760, 10);
+            this.startButton.Size = new System.Drawing.Size(100, 35);
+            this.startButton.BackColor = System.Drawing.Color.LightGreen;
+            this.startButton.Click += new EventHandler(this.StartButton_Click);
+
+            // Кнопка Сброс
+            this.resetButton = new Button();
+            this.resetButton.Text = "Сброс";
+            this.resetButton.Location = new System.Drawing.Point(870, 10);
+            this.resetButton.Size = new System.Drawing.Size(100, 35);
+            this.resetButton.BackColor = System.Drawing.Color.LightCoral;
+            this.resetButton.Click += new EventHandler(this.ResetButton_Click);
+
+            // Статусная метка
+            this.statusLabel = new Label();
+            this.statusLabel.Text = "Готов";
+            this.statusLabel.Location = new System.Drawing.Point(10, 180);
+            this.statusLabel.Size = new System.Drawing.Size(300, 25);
+            this.statusLabel.Font = new System.Drawing.Font("Arial", 10, System.Drawing.FontStyle.Bold);
+
+            // Группа легенды
+            GroupBox legendBox = new GroupBox();
+            legendBox.Text = "Легенда";
+            legendBox.Location = new System.Drawing.Point(990, 10);
+            legendBox.Size = new System.Drawing.Size(160, 180);
+
+            // Добавление элементов легенды
+            AddLegendItem(legendBox, System.Drawing.Color.SteelBlue, "Обычный элемент", 0);
+            AddLegendItem(legendBox, System.Drawing.Color.Orange, "Сравниваемый", 1);
+            AddLegendItem(legendBox, System.Drawing.Color.Red, "Перемещаемый", 2);
+            AddLegendItem(legendBox, System.Drawing.Color.LightGreen, "Отсортированный", 3);
+            AddLegendItem(legendBox, System.Drawing.Color.Purple, "Опорный элемент", 4);
+
+            // Добавление всех контролов на панель управления
+            this.controlPanel.Controls.AddRange(new System.Windows.Forms.Control[] {
+                algoLabel, this.algorithmCombo, sizeLabel, this.sizeNumeric,
+                speedLabel, this.speedTrackBar, this.startButton, this.resetButton,
+                this.statusLabel, legendBox
+            });
+
+            // Добавление панелей на форму
+            this.Controls.Add(this.canvas);
+            this.Controls.Add(this.controlPanel);
+
+            // Настройка компонентов
+            this.components = new System.ComponentModel.Container();
+        }
+
+        private void AddLegendItem(GroupBox box, System.Drawing.Color color, string text, int index)
+        {
+            Panel colorPanel = new Panel();
+            colorPanel.Location = new System.Drawing.Point(10, index * 25 + 25);
+            colorPanel.Size = new System.Drawing.Size(20, 20);
+            colorPanel.BackColor = color;
+            colorPanel.BorderStyle = BorderStyle.FixedSingle;
+
+            Label label = new Label();
+            label.Text = text;
+            label.Location = new System.Drawing.Point(35, index * 25 + 25);
+            label.Size = new System.Drawing.Size(120, 20);
+            label.Font = new System.Drawing.Font("Arial", 8);
+
+            box.Controls.Add(colorPanel);
+            box.Controls.Add(label);
+        }
+
+        // Обработчики событий
+        private void SizeNumeric_ValueChanged(object sender, EventArgs e)
+        {
+            if (!isSorting) GenerateArray();
+        }
+
+        private void ResetButton_Click(object sender, EventArgs e)
+        {
+            if (!isSorting) GenerateArray();
+        }
+    }
+}
---
+++ Ргр/MainForm.cs
@@ -0,0 +1,501 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace РГР
+{
+    public partial class MainForm : Form
+    {
+        private Panel canvas;
+        private ComboBox algorithmCombo;
+        private Button startButton;
+        private Button resetButton;
+        private TrackBar speedTrackBar;
+        private NumericUpDown sizeNumeric;
+        private Label statusLabel;
+
+        private int[] array;
+        private int arraySize = 30;
+        private int maxValue = 100;
+        private bool isSorting = false;
+        private CancellationTokenSource cancellationTokenSource;
+
+        // Цвета для отображения
+        private Color defaultColor = Color.SteelBlue;
+        private Color comparingColor = Color.Orange;
+        private Color swappingColor = Color.Red;
+        private Color sortedColor = Color.LightGreen;
+        private Color pivotColor = Color.Purple;
+
+        // Для отображения элементов вне массива
+        private int? externalElement1 = null;
+        private int? externalElement2 = null;
+        private int? externalElementIndex1 = null;
+        private int? externalElementIndex2 = null;
+        private int sortedCount = 0;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            this.Resize += MainForm_Resize;
+            GenerateArray();
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            // Адаптация размеров при изменении окна
+            if (canvas != null)
+            {
+                canvas.Width = this.ClientSize.Width - 40;
+                canvas.Height = 400;
+                controlPanel.Top = canvas.Bottom + 10;
+                controlPanel.Width = this.ClientSize.Width - 40;
+            }
+        }
+
+        private void GenerateArray()
+        {
+            arraySize = (int)sizeNumeric.Value;
+            Random rand = new Random();
+            array = new int[arraySize];
+            for (int i = 0; i < arraySize; i++)
+            {
+                array[i] = rand.Next(10, maxValue + 10);
+            }
+            externalElement1 = null;
+            externalElement2 = null;
+            sortedCount = 0;
+            canvas.Invalidate();
+        }
+
+        private void Canvas_Paint(object sender, PaintEventArgs e)
+        {
+            if (array == null || array.Length == 0) return;
+
+            Graphics g = e.Graphics;
+            int barWidth = canvas.Width / array.Length - 2;
+            if (barWidth < 1) barWidth = 1;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int barHeight = (int)((double)array[i] / maxValue * (canvas.Height - 50));
+                int x = i * (barWidth + 2) + 2;
+                int y = canvas.Height - barHeight - 20;
+
+                Color barColor = defaultColor;
+
+                if (externalElementIndex1 == i || externalElementIndex2 == i)
+                {
+                    barColor = Color.Transparent;
+                }
+                else if (i < sortedCount)
+                {
+                    barColor = sortedColor;
+                }
+                else
+                {
+                    barColor = defaultColor;
+                }
+
+                using (Brush brush = new SolidBrush(barColor))
+                {
+                    g.FillRectangle(brush, x, y, barWidth, barHeight);
+                }
+
+                g.DrawRectangle(Pens.Black, x, y, barWidth, barHeight);
+
+                using (Font font = new Font("Arial", 8))
+                {
+                    string valueText = array[i].ToString();
+                    SizeF textSize = g.MeasureString(valueText, font);
+                    g.DrawString(valueText, font, Brushes.Black,
+                        x + barWidth / 2 - textSize.Width / 2, y - 15);
+                }
+            }
+
+            if (externalElement1.HasValue)
+            {
+                DrawExternalElement(g, externalElement1.Value, 50, 20, comparingColor, "Элемент 1");
+            }
+            if (externalElement2.HasValue)
+            {
+                DrawExternalElement(g, externalElement2.Value, 50, 70, swappingColor, "Элемент 2");
+            }
+        }
+
+        private void DrawExternalElement(Graphics g, int value, int x, int y, Color color, string label)
+        {
+            int barWidth = 40;
+            int barHeight = (int)((double)value / maxValue * 80);
+
+            using (Brush brush = new SolidBrush(color))
+            {
+                g.FillRectangle(brush, x, y + 50 - barHeight, barWidth, barHeight);
+            }
+            g.DrawRectangle(Pens.Black, x, y + 50 - barHeight, barWidth, barHeight);
+            g.DrawString(value.ToString(), new Font("Arial", 10, FontStyle.Bold), Brushes.Black,
+                x + barWidth / 2 - 8, y + 50 - barHeight - 15);
+            g.DrawString(label, new Font("Arial", 8), Brushes.Black, x, y);
+        }
+
+        private async void StartButton_Click(object sender, EventArgs e)
+        {
+            if (isSorting) return;
+
+            isSorting = true;
+            startButton.Enabled = false;
+            resetButton.Enabled = false;
+            algorithmCombo.Enabled = false;
+            sizeNumeric.Enabled = false;
+            sortedCount = 0;
+            externalElement1 = null;
+            externalElement2 = null;
+
+            cancellationTokenSource = new CancellationTokenSource();
+
+            try
+            {
+                switch (algorithmCombo.SelectedIndex)
+                {
+                    case 0:
+                        await BubbleSort();
+                        break;
+                    case 1:
+                        await SelectionSort();
+                        break;
+                    case 2:
+                        await InsertionSort();
+                        break;
+                    case 3:
+                        await MergeSort(0, array.Length - 1);
+                        break;
+                    case 4:
+                        await QuickSort(0, array.Length - 1);
+                        break;
+                    case 5:
+                        await TreeSort();
+                        break;
+                }
+                sortedCount = array.Length;
+                canvas.Invalidate();
+                statusLabel.Text = "Сортировка завершена!";
+            }
+            catch (OperationCanceledException)
+            {
+                statusLabel.Text = "Сортировка прервана";
+            }
+            finally
+            {
+                isSorting = false;
+                startButton.Enabled = true;
+                resetButton.Enabled = true;
+                algorithmCombo.Enabled = true;
+                sizeNumeric.Enabled = true;
+                externalElement1 = null;
+                externalElement2 = null;
+                canvas.Invalidate();
+            }
+        }
+
+        private async Task Delay()
+        {
+            await Task.Delay(speedTrackBar.Value);
+        }
+
+        private async Task ShowComparison(int index1, int index2, bool showExternally = true)
+        {
+            if (showExternally && index2 >= 0)
+            {
+                externalElementIndex1 = index1;
+                externalElementIndex2 = index2;
+                externalElement1 = array[index1];
+                externalElement2 = array[index2];
+            }
+            canvas.Invalidate();
+            await Delay();
+        }
+
+        private void ClearExternal()
+        {
+            externalElement1 = null;
+            externalElement2 = null;
+            externalElementIndex1 = null;
+            externalElementIndex2 = null;
+            canvas.Invalidate();
+        }
+
+        private async Task Swap(int i, int j)
+        {
+            await ShowComparison(i, j);
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+            ClearExternal();
+            canvas.Invalidate();
+            await Delay();
+        }
+
+        private async Task BubbleSort()
+        {
+            statusLabel.Text = "Пузырьковая сортировка...";
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                bool swapped = false;
+                for (int j = 0; j < array.Length - 1 - i; j++)
+                {
+                    await ShowComparison(j, j + 1);
+                    if (array[j] > array[j + 1])
+                    {
+                        await Swap(j, j + 1);
+                        swapped = true;
+                    }
+                    ClearExternal();
+                }
+                sortedCount = array.Length - 1 - i;
+                canvas.Invalidate();
+                if (!swapped) break;
+            }
+            sortedCount = array.Length;
+        }
+
+        private async Task SelectionSort()
+        {
+            statusLabel.Text = "Сортировка выбором...";
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                int minIdx = i;
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    await ShowComparison(minIdx, j);
+                    if (array[j] < array[minIdx])
+                    {
+                        minIdx = j;
+                    }
+                    ClearExternal();
+                }
+                if (minIdx != i)
+                {
+                    await Swap(i, minIdx);
+                }
+                sortedCount = i + 1;
+                canvas.Invalidate();
+            }
+        }
+
+        private async Task InsertionSort()
+        {
+            statusLabel.Text = "Сортировка вставками...";
+            for (int i = 1; i < array.Length; i++)
+            {
+                int key = array[i];
+                int j = i - 1;
+
+                externalElement1 = key;
+                externalElementIndex1 = i;
+                canvas.Invalidate();
+                await Delay();
+
+                while (j >= 0 && array[j] > key)
+                {
+                    await ShowComparison(j, -1, false);
+                    array[j + 1] = array[j];
+                    canvas.Invalidate();
+                    await Delay();
+                    j--;
+                }
+                array[j + 1] = key;
+                ClearExternal();
+                sortedCount = i + 1;
+                canvas.Invalidate();
+                await Delay();
+            }
+        }
+
+        private async Task MergeSort(int left, int right)
+        {
+            if (left < right)
+            {
+                int mid = (left + right) / 2;
+                await MergeSort(left, mid);
+                await MergeSort(mid + 1, right);
+                await Merge(left, mid, right);
+            }
+        }
+
+        private async Task Merge(int left, int mid, int right)
+        {
+            int n1 = mid - left + 1;
+            int n2 = right - mid;
+
+            int[] L = new int[n1];
+            int[] R = new int[n2];
+
+            for (int i = 0; i < n1; i++) L[i] = array[left + i];
+            for (int j = 0; j < n2; j++) R[j] = array[mid + 1 + j];
+
+            int iIdx = 0, jIdx = 0, k = left;
+
+            while (iIdx < n1 && jIdx < n2)
+            {
+                externalElement1 = L[iIdx];
+                externalElement2 = R[jIdx];
+                canvas.Invalidate();
+                await Delay();
+
+                if (L[iIdx] <= R[jIdx])
+                {
+                    array[k] = L[iIdx];
+                    iIdx++;
+                }
+                else
+                {
+                    array[k] = R[jIdx];
+                    jIdx++;
+                }
+                canvas.Invalidate();
+                await Delay();
+                k++;
+            }
+
+            while (iIdx < n1)
+            {
+                array[k] = L[iIdx];
+                iIdx++;
+                k++;
+                canvas.Invalidate();
+                await Delay();
+            }
+
+            while (jIdx < n2)
+            {
+                array[k] = R[jIdx];
+                jIdx++;
+                k++;
+                canvas.Invalidate();
+                await Delay();
+            }
+
+            ClearExternal();
+            sortedCount = right + 1;
+            canvas.Invalidate();
+        }
+
+        private async Task QuickSort(int low, int high)
+        {
+            if (low < high)
+            {
+                int pi = await Partition(low, high);
+                await QuickSort(low, pi - 1);
+                await QuickSort(pi + 1, high);
+            }
+            else if (low == high)
+            {
+                sortedCount = Math.Max(sortedCount, low + 1);
+                canvas.Invalidate();
+            }
+        }
+
+        private async Task<int> Partition(int low, int high)
+        {
+            int pivot = array[high];
+            int i = low - 1;
+
+            externalElement1 = pivot;
+            externalElementIndex1 = high;
+            canvas.Invalidate();
+            await Delay();
+
+            for (int j = low; j < high; j++)
+            {
+                await ShowComparison(j, high);
+                if (array[j] <= pivot)
+                {
+                    i++;
+                    await Swap(i, j);
+                }
+                ClearExternal();
+            }
+            await Swap(i + 1, high);
+            ClearExternal();
+            sortedCount = i + 2;
+            canvas.Invalidate();
+            return i + 1;
+        }
+
+        private async Task TreeSort()
+        {
+            statusLabel.Text = "Древесная сортировка (построение дерева)...";
+
+            TreeNode root = null;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                externalElement1 = array[i];
+                externalElementIndex1 = i;
+                canvas.Invalidate();
+                await Delay();
+
+                root = InsertIntoTree(root, array[i]);
+                ClearExternal();
+                await Delay();
+            }
+
+            statusLabel.Text = "Древесная сортировка (обход дерева)...";
+
+            List<int> sortedList = new List<int>();
+            await InorderTraversal(root, sortedList);
+
+            for (int i = 0; i < sortedList.Count; i++)
+            {
+                array[i] = sortedList[i];
+                sortedCount = i + 1;
+                canvas.Invalidate();
+                await Delay();
+            }
+        }
+
+        private TreeNode InsertIntoTree(TreeNode root, int value)
+        {
+            if (root == null)
+                return new TreeNode(value);
+
+            if (value < root.Value)
+                root.Left = InsertIntoTree(root.Left, value);
+            else
+                root.Right = InsertIntoTree(root.Right, value);
+
+            return root;
+        }
+
+        private async Task InorderTraversal(TreeNode node, List<int> result)
+        {
+            if (node != null)
+            {
+                await InorderTraversal(node.Left, result);
+                result.Add(node.Value);
+                await Delay();
+                await InorderTraversal(node.Right, result);
+            }
+        }
+
+        private class TreeNode
+        {
+            public int Value { get; set; }
+            public TreeNode Left { get; set; }
+            public TreeNode Right { get; set; }
+
+            public TreeNode(int value)
+            {
+                Value = value;
+                Left = null;
+                Right = null;
+            }
+        }
+
+        // Панель управления (добавляем как поле класса)
+        private Panel controlPanel;
+    }
+}
